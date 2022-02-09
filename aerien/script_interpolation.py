7fe1d7a312eaed8bbaae209521cb9d68e639b1eb
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2022-02-09 10:27:32 +0100

    Première version stable
    Parallélisation de l'interpolation

--- aerien/script_interpolation.py
+++ aerien/script_interpolation.py
@@ -2,33 +2,36 @@ from grass.pygrass.modules import Module
 from grass.pygrass.gis.region import Region
 from grass.script import core as grass
 from multiprocessing import Process
-import json
+import json
+
 import time
 
 with open("D:\\Documents\\GitHub\\scan_process\\aerien\\config.json") as jsonFile:
     config = json.load(jsonFile)
-    jsonFile.close()
-
-def check_values():
-    config_region = config.get("interpolation").get("g.region")
-    config_surf = config.get("interpolation").get("v.surf.rst")
-
-    buffer = config_region.get("grow")
-    npmin = config_surf.get("npmin")
-    dmin = config_surf.get("dmin")
+    jsonFile.close()
+
+def check_values():
+    config_region = config.get("interpolation").get("g.region")
+    config_surf = config.get("interpolation").get("v.surf.rst")
+
+    buffer = config_region.get("grow")
+    npmin = config_surf.get("npmin")
+    dmin = config_surf.get("dmin")
     segmax = config_surf.get("segmax")
-
-    if(buffer < npmin):
-        print("La valeur grow n'est pas coherente.")
-        return False
-    elif(segmax > npmin):
-        print("La valeur segmax doit etre inferieur a la valeur npmin.")
-        return False
-    elif(buffer < npmin * dmin):
-        print("La valeur grow doit etre superieure ou egale au produit npmin * dmin.")
-        return False
-    else:
-        return True
+    res3 = config_region.get("res3")
+
+    if(buffer * res3 < npmin):
+        print("La valeur grow n'est pas coherente.")
+        return False
+    elif(segmax > npmin):
+        print("La valeur segmax doit etre inferieur a la valeur npmin.")
+        return False
+    elif(buffer * res3 < npmin * dmin):
+        print("La valeur grow doit etre superieure ou egale au produit npmin * dmin.")
+        return False
+    else:
+        return True
+
 
 #Permet de creer une nouvelle une nouvelle location et un nouveau mapset dans WinGRASS
 def create_new_location():
@@ -45,7 +48,7 @@ def create_new_location():
 def import_file():
     config_import = config.get("import_file").get("v.in.lidar")
     Module("v.in.lidar",
-           flags="tbo",
+           flags="tboe",
            input=config_import.get("input"),
            output=config_import.get("output"),
            class_filter=config_import.get("class_filter"),
@@ -59,11 +62,9 @@ def create_region(regions, i, nRegion):
     config_gdal = config.get("interpolation").get("r.out.gdal")
 
     r = Region()
-    r.vector = config_region.get("vector")
-    r.overwrite = True
 
     xDist = r.east - r.west
-    yDist = r.north - r.south
+    yDist = r.north - r.south
 
 #Partie superieure du jeu de donnees
     if(nRegion < regions/2):
@@ -72,15 +73,15 @@ def create_region(regions, i, nRegion):
             "g.region",
             flags = "pu",
             vector = config_region.get("vector"), 
-            res3 = config_region.get("res"),
+            res3 = config_region.get("res3"),
             n = r.north,
             s = r.south + yDist/2,
             e = r.east - (((regions/2 - 1)-i) * (xDist/(regions/2))),
             w = r.west + i * (xDist/(regions/2)),
             grow = config_region.get("grow")
-        )
+        )
 
-        #Interpolation
+        #Interpolation
         Module(
             "v.surf.rst",
             input = config_surf.get("input"),
@@ -89,7 +90,7 @@ def create_region(regions, i, nRegion):
             segmax = config_surf.get("segmax"),
             npmin = config_surf.get("npmin"),
             overwrite = True
-        )
+        )
 
     #Partie inferieure du jeu de donnees
     else:
@@ -98,12 +99,12 @@ def create_region(regions, i, nRegion):
             "g.region",
             flags = "pu",
             vector = config_region.get("vector"), 
-            res = config_region.get("res"),
             res3 = config_region.get("res"),
             n = r.north - yDist/2,
             s = r.south,
             e = r.east - (((regions/2 - 1)-i) * (xDist/(regions/2))),
-            w = r.west + i * (xDist/(regions/2)),
+            w = r.west + i * (xDist/(regions/2)),
+            grow = config_region.get("grow")
         )
         #Interpolation
         Module(
@@ -121,27 +122,29 @@ def create_region(regions, i, nRegion):
     Module(
         "r.out.gdal",
         input = "region_" + str(nRegion) + "_elevation",
-        output = "D:\\Documents\\MISSIONS\\interpolation_raster\\output\\" + str(nRegion) + "_output.tif",
+        output = config_gdal.get("output") + str(nRegion) + "_output.tif",
         format = config_gdal.get("format"),
         overwrite = True
     )
 
-if __name__ == "__main__":
+if __name__ == "__main__":
     valuesOk = check_values()
-    if(valuesOk):
-        #create_new_location()
-        #import_file()
-        regions = 4
+    if(valuesOk):
+        create_new_location()
+        import_file()
+        regions = input("En combien de régions voulez vous découper le vecteur ?")
 
         i = 0
-
-        p = Process(target=create_region, args=(regions, 0, 0))
-        p.start()
+        processes = []
 
         for j in range(regions):
             p = Process(target=create_region, args=(regions, i, j))
-            p.start()
+            p.start()
+            processes.append(p)
             if i < (regions / 2) - 1:
                 i += 1
             else:
-                i = 0
+                i = 0
+        
+        for p in processes:
+            p.join()
